1c8f54f907a49e5b5ff1ff1589679e053f4e742b
ordermasho01-boop <order.masho01@gmail.com>
2025-08-20 04:22:38 +0000

    navbar and sibar data created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,19 +6,19 @@ const Navbar = ({activeMenu}) => {
   const [open, setOpen]= useState(false)
 
   return (
-    <div className='flex bg-white borser border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
-      <button className='block lg:hidden text-black'
+    <div className='flex gap-5 bg-white borser border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
+      <button className='block lg:hidden text-black '
       onClick={()=>setOpen(!open)}>
         {open ? (
-          <HiOutlineX className='text-2xl'/>
+          <HiOutlineX className='text-2xl bg-white '/>
         ):(
-          <HiOutlineMenu className='text-2xl' />
+          <HiOutlineMenu className='text-2xl bg-white' />
         )}
       </button>
-      <h2 className='text-lg font-medium text-black'>Expense Tracker</h2>
+      <h2 className='text-lg font-medium text-black bg-white'>Expense Tracker</h2>
       {open && (
         <div className='fixed top-[61px] -ml-4 bg-white'>
-          <Sidebar activeMenu= {activeMenu}/>
+          <Sidebar activeMenu= {activeMenu} />
         </div>
       )}
     </div>
---
+++ client/src/utils/data.js
@@ -0,0 +1,32 @@
+import {
+  LuHandCoins,
+  LuLayoutDashboard,
+  LuLogOut,
+  LuWalletMinimal,
+} from "react-icons/lu";
+export const SIDE_BAR_DATA = [
+  {
+    id: "01",
+    label: "Dashboard",
+    icon: LuLayoutDashboard,
+    path: "/dashboard",
+  },
+  {
+    id: "02",
+    label: "Income",
+    icon: LuWalletMinimal,
+    path: "/income",
+  },
+  {
+    id: "03",
+    label: "Expense",
+    icon: LuHandCoins,
+    path: "/expense",
+  },
+  {
+    id: "04",
+    label: "Logout",
+    icon: LuLogOut,
+    path: "logout",
+  },
+];
